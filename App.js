b480b73db5a6a663217e56525b4d92c5211749f8
salonidarji <salonidarji@gmail.com>
2019-06-13 11:15:47 +0530

    initial commit with Four Screens

---
+++ App.js
@@ -0,0 +1,29 @@
+import { HomeScreen } from "./screens/HomeScreen";
+import { AddNewContactScreen } from "./screens/AddNewContactScreen";
+import { EditContactScreen } from "./screens/EditContactScreen";
+import { ViewContactScreen } from "./screens/ViewContactScreen";
+
+import { createStackNavigator, createAppContainer } from "react-navigation";
+
+const MainNavigator = createStackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    Add: { screen: AddNewContactScreen },
+    View: { screen: EditContactScreen },
+    Edit: { screen: ViewContactScreen }
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: "#fff",
+      headerStyle: {
+        backgroundColor: "#fff"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      }
+    }
+  }
+);
+
+const App = createAppContainer(MainNavigator);
+export default App;
